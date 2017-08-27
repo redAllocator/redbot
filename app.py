fc80ef67a977c11e8de5603073dd6da452c72496
redAllocator <red.allocator@gmail.com>
2017-08-27 12:51:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,57 @@
-from flask import Flask
+# -*- coding: utf-8 -*- 
+import os
 from datetime import datetime
+from flask import Flask, request, jsonify
+  
+
 app = Flask(__name__)
+ 
+
+@app.route('/keyboard')
+def Keyboard():
+ 
+    dataSend = {
+        "type" : "buttons",
+        "buttons" : ["시작하기", "도움말"]
+    }
+ 
+    return jsonify(dataSend)
+ 
+ 
+ 
+@app.route('/message', methods=['POST'])
+def Message():
+    
+    dataReceive = request.get_json()
+    content = dataReceive['content']
+ 
+    if content == u"시작하기":
+        dataSend = {
+            "message": {
+                "text": "아직 개발중이라 대답을 잘 못해도 이해해줘^^;"
+            }
+        }
+    elif content == u"도움말":
+        dataSend = {
+            "message": {
+                "text": "이제 곧 정식 버전이 출시될거야. 조금만 기다려~~~"
+            }
+        }
+    elif u"안녕" in content:
+        dataSend = {
+            "message": {
+                "text": "안녕~~ 반가워 ㅎㅎ"
+            }
+        }
+    else:
+        dataSend = {
+            "message": {
+                "text": "나랑 놀자 ㅋㅋㅋ"
+            }
+        }
+ 
+    return jsonify(dataSend)
+ 
 
 @app.route('/')
 def homepage():
@@ -10,6 +61,5 @@ def homepage():
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
     """.format(time=the_time)
-
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
