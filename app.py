033a49ae8ec487811e34bd1eeed0c587e600861d
redAllocator <red.allocator@gmail.com>
2017-08-31 23:25:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,25 +28,37 @@ def Message():
     if content == u"시작하기":
         dataSend = {
             "message": {
-                "text": "아직 개발중이라 대답을 잘 못해도 이해해줘^^;"
+                "text": "실시간 도로소통정보를 제공하는 carvis 서비스입니다"
             }
         }
     elif content == u"도움말":
         dataSend = {
             "message": {
-                "text": "이제 곧 정식 버전이 출시될거야. 조금만 기다려~~~"
+                "text": "#소통, #돌발(사고, 공사 행사), #통제"
+            }
+        }
+    elif u"서울" in content:
+        dataSend = {
+            "message": {
+                "text": "서울외곽순환고속도로 성남톨게이트→성남IC 기간 2017.08.31. 22:48 ~ 2017.09.01. 01:51 내용<공사> 서울외곽순환선(1000) (2,3차로)노면 보수 작업중"
+            }
+        }
+   elif u"소통" in content:
+        dataSend = {
+            "message": {
+                "text": "퇴계로2가->충무로역 적재불량 초보자 트럭 발견"
             }
         }
     elif u"안녕" in content:
         dataSend = {
             "message": {
-                "text": "안녕~~ 반가워 ㅎㅎ"
+                "text": "안녕하세요 carvis입니다.^^"
             }
-        }
+    }
     else:
         dataSend = {
             "message": {
-                "text": "나랑 놀자 ㅋㅋㅋ"
+                "text": "carvis 학습중"
             }
         }
  
